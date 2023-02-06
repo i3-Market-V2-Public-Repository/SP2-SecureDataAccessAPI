fcc54c8e64db3a6651b07902d83c2e6effd441bf
IosifFurtuna <furtuna.iosif@yahoo.com>
2023-02-06 13:50:40 +0200

    vulns update

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ COPY . /secure-data-access-api/
 WORKDIR /secure-data-access-api
 
 RUN chmod +x ./scripts/dependencies.sh && bash ./scripts/dependencies.sh
+RUN chmod +x ./scripts/library_update.sh && bash ./scripts/library_update.sh
 
 CMD ["npm", "start"]
---
+++ scripts/library_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update -y && \
+apt-get install -y \
+curl=7.64.0-4+deb10u4 \
+git=1:2.20.1-2+deb10u7 \
+git-man=1:2.20.1-2+deb10u7 \
+libcurl3-gnutls=7.64.0-4+deb10u4 \
+libcurl4=7.64.0-4+deb10u4 \
+libcurl4-openssl-dev=7.64.0-4+deb10u4 \
+libde265-0=1.0.3-1+deb10u3 \
+libtiff-dev=4.1.0+git191117-2~deb10u6 \
+libtiff5=4.1.0+git191117-2~deb10u6 \
+libtiffxx5=4.1.0+git191117-2~deb10u6
